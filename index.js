8a24c5c8f03037429c5b2731831e34768016dbfb
Sebastian Martinez <isaacsoccer235@gmail.com>
2023-04-29 21:22:21 -0500

    Objects Lab :///

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+const employee = {
+    name: 'Sam',
+    streetAddress: '11th Broadway St',
+};
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+console.log("employee", employee)
+let newEmployee = {...employee}
+newEmployee[key] = value;
+return newEmployee;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key) {
+    let newEmployee = {...employee}
+    delete newEmployee.name
+    return newEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee.name
+    return employee;
+}
